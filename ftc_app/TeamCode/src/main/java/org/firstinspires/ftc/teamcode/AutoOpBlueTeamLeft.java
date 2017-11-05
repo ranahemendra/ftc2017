ae19089d54f3931bacd338c5d41d13b7a0b176b8
Howard Huang <schoolproject4fun@gmail.com>
2017-11-04 18:02:36 -0700

    AutoOpBlueTeamRight - jewel knock off, and glyph placement according to VuMarks. Adjusted distance values

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpBlueTeamLeft.java
@@ -0,0 +1,112 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+@Autonomous(name = "Blue Team Left", group = "Autonomous")
+public class AutoOpBlueTeamLeft extends AutoOpBase {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initBot();
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // scan VuMark
+        bot.relicTrackables.activate();
+        RelicRecoveryVuMark scannedVuMark = scanVumarks(1);
+
+        bot.moveJewelKnockerDown();
+        // Wait for the servo to move.
+        sleep(600);
+
+
+        // scans right jewel
+        boolean leftJewelRed = isLeftJewelRed();
+
+        if (leftJewelRed) {
+            // move forward
+            driveBackwardDistance(3, bot.AUTO_DRIVE_SPEED_SLOW);
+            bot.resetJewelKnocker();
+            sleep(500);
+            driveBackwardDistance(20, bot.AUTO_DRIVE_SPEED_SLOW);
+        } else {
+            driveForwardDistance(3, bot.AUTO_DRIVE_SPEED_SLOW);
+            bot.resetJewelKnocker();
+            sleep(500);
+            driveBackwardDistance(26, bot.AUTO_DRIVE_SPEED_SLOW);
+        }
+
+        // Go back a few inches to align with the balancing stone
+        driveForwardDistance(8, bot.AUTO_DRIVE_SPEED_NORMAL);
+
+        // Come back to the original position.
+        driveBackwardDistance(6, bot.AUTO_DRIVE_SPEED_NORMAL);
+
+        // Turn Left.
+        turnLeftToAngle(85);
+
+        //Go Back so turn will not move the bot forward so much that it misses the cryptobox slot.
+        driveBackwardDistance(9, bot.AUTO_DRIVE_SPEED_NORMAL);
+
+        telemetry.addData("Vumark", scannedVuMark);
+
+        int driveDistance = 3;
+        if(scannedVuMark == scannedVuMark.RIGHT) {
+            driveDistance += 14;
+        } else if (scannedVuMark == scannedVuMark.CENTER) {
+            driveDistance += 7;
+        } else if (scannedVuMark == scannedVuMark.LEFT) {
+            // Don't do anything.
+        }
+
+        // Turn Left.
+        turnLeftToAngle(82);
+        driveForwardDistance(5, bot.AUTO_DRIVE_SPEED_SLOW);
+        // Drive forward to the cryptobox
+
+
+        bot.unclampGlyph();
+
+        driveBackwardDistance(4, bot.AUTO_DRIVE_SPEED_NORMAL);
+
+        telemetry.addData("Time taken", getRuntime());
+        telemetry.update();
+
+        while (opModeIsActive()) {
+            // Keep this going.
+            idle();
+        }
+    }
+}
